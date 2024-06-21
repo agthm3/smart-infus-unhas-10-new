3f70501bc682d8ae04332f105b077609311396de
agthm3 <gigam57@gmail.com>
2024-06-21 15:35:22 -0700

    add dummy alert to dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -4,6 +4,27 @@
 {{-- @dd($alertPatients) --}}
     <div class="container-xxl flex-grow-1 container-p-y">
         <div class="row">
+            <div class="col-lg-12 mb-4 order-0">
+                <div class="card">
+                    <div class="d-flex align-items-end row">
+                        <div class="col-sm-7">
+                            <div class="card-body">
+                                <h5 class="card-title text-primary">
+                                    <span><strong class="text-danger">Peringatan!</strong></span>
+                                </h5>
+                                <p class="mb-4">Infus Pasien Dinda, telah lewat 3 menit!</p>
+                            </div>
+                        </div>
+                        <div class="col-sm-5 text-center text-sm-left">
+                            <div class="card-body pb-0 px-0 px-md-4">
+                                <img src="../assets/img/illustrations/undraw_warning_re_eoyh.svg" height="140"
+                                    alt="View Badge User" data-app-dark-img="illustrations/man-with-laptop-dark.png"
+                                    data-app-light-img="illustrations/man-with-laptop-light.png" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
           @foreach ($alertPatients as $item)
           <div class="col-lg-12 mb-4 order-0">
             <div class="card">
